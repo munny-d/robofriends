554fef9c5bd98667a7c5748629cb35a97c0bfe3d
munny-d <munny_d@hotmail.co.uk>
2020-02-06 17:34:11 +0000

    my first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# zigzagrobo"# robofriends" 
---
+++ src/robots.js
@@ -0,0 +1,80 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Munny',
+    username: 'Bret',
+    email: 'email@email.com'
+  },
+  {
+    id: 2,
+    name: 'AJ',
+    username: 'Antonette',
+    email: 'email@email.com'
+  },
+  {
+    id: 3,
+    name: 'Harry',
+    username: 'Samantha',
+    email: 'email@email.com'
+  },
+  {
+    id: 4,
+    name: 'Martin',
+    username: 'Karianne',
+    email: 'email@email.com'
+  },
+  {
+    id: 5,
+    name: 'Huong',
+    username: 'Kamren',
+    email: 'email@email.com'
+  },
+  {
+    id: 6,
+    name: 'Artur',
+    username: 'Leopoldo_Corkery',
+    email: 'email@email.com'
+  },
+  {
+    id: 7,
+    name: 'Irfan',
+    username: 'Elwyn.Skiles',
+    email: 'email@email.com'
+  },
+  {
+    id: 8,
+    name: 'Matt',
+    username: 'Maxime_Nienow',
+    email: 'email@email.com'
+  },
+  {
+    id: 9,
+    name: 'Pippa',
+    username: 'Delphine',
+    email: 'email@email.com'
+  },
+  {
+    id: 10,
+    name: 'Yurii',
+    username: 'Moriah.Stanton',
+    email: 'email@email.com'
+  },
+  {
+    id: 11,
+    name: 'Liudmyla',
+    username: 'Moriah.Stanton',
+    email: 'email@email.com'
+  },
+  {
+    id: 12,
+    name: 'Graham',
+    username: 'Moriah.Stanton',
+    email: 'email@email.com'
+  },
+  {
+    id: 13,
+    name: 'Andrii',
+    username: 'Moriah.Stanton',
+    email: 'email@email.com'
+  }
+];
